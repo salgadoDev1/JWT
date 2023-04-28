75fdb49ab0e9878e066f7e2c2beee40b39e216a2
Jose Salgado <you@example.com>
2023-04-28 01:24:00 -0400

    dependencias JWT

--- config/app.php
+++ config/app.php
@@ -195,6 +195,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //JWT
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class
+
     ],
 
     /*
